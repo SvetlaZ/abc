79ffc6b5e2d8a2f6210ac1225ea95f3de2601275
Svetlana <svezan@mail.ru>
2021-01-18 19:49:32 +0300

    add validate for form

--- src/index.js
+++ src/index.js
@@ -31,3 +31,52 @@ if (animItems.length) {
   window.addEventListener('scroll', animOnScroll);
   animOnScroll();
 }
+
+function formAddError(input) {
+  input.parentElement.classList.add('_error');
+  input.classList.add('_error');
+}
+
+function formRemoveError(input) {
+  input.parentElement.classList.remove('_error');
+  input.classList.remove('_error');
+}
+
+function telTest(input) {
+  return !/\+?\d{1}[(]\d{3}[)]\d{3}[-]\d{2}[-]\d{2}/.test(input.value);
+}
+
+function formValidate(form) {
+  let error = 0;
+  let formReq = document.querySelectorAll('._req');
+console.log('formReq: ', formReq);
+  for (let i = 0; i < formReq.length; i += 1) {
+    const input = formReq[i];
+    formRemoveError(input);
+
+    if (input.classList.contains('_tel')) {
+      if (telTest(input)) {
+        formAddError(input);
+        error += 1;
+      }
+    } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+      formAddError(input);
+      error += 1;
+    } else if (input.value === '') {
+      formAddError(input);
+      error += 1;
+    }
+  }
+  console.log('error: ', error);
+}
+
+const form = document.getElementById('form');
+
+async function formSend(e) {
+  e.preventDefault();
+  
+  let error = formValidate(form);
+  
+}
+
+form.addEventListener('submit', formSend);
