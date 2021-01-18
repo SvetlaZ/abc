28414e030e3a0f73a255a8a3c5ac92be40533f2a
Svetlana <svezan@mail.ru>
2021-01-18 12:49:43 +0300

    add animation for gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './style.scss';
+
+const animItems = document.querySelectorAll('._anim-items');
+
+function offset(el) {
+  const rect = el.getBoundingClientRect();
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+}
+
+function animOnScroll() {
+  for (let i = 0; i < animItems.length; i += 1) {
+    const animItem = animItems[i];
+    const animItemHeight = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = 4;
+
+    const animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+    if ((pageYOffset > animItemOffset - animItemPoint)
+    && pageYOffset < (animItemOffset + animItemHeight)) {
+      animItem.classList.add('_active');
+    } else {
+      animItem.classList.remove('_active');
+    }
+  }
+}
+
+if (animItems.length) {
+  window.addEventListener('scroll', animOnScroll);
+  animOnScroll();
+}
